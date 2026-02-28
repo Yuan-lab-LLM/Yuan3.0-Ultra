a9dbab9d55bb87f5eda87c21245e3e56002284e2
hehoubo <hehoubo@inspur.com>
2026-02-28 16:21:40 +0800

    add eval files

---
+++ vllm/eval/inference/generate.sh
@@ -0,0 +1,1 @@
+bfcl generate --model model_path --test-category all --num-threads 16 --include-input-log 
---
+++ vllm/eval/scripts/ChatRAG/eval_chatqa.sh
@@ -0,0 +1,2 @@
+python3 ChatQA/get_scores1.py --base-path eval/result/rag_en --row 2 --output "ChatQA/test1.xlsx"
+
